f012672cfb0c0eb0cd3913f670fb5ce1eae9137c
Ryan Kirkwood <rkirkwood@rand-associates.co.uk>
2022-07-03 09:59:16 +0100

    folder structure.

--- Logic/ToDoActions.cs
+++ Logic/ToDoActions.cs
@@ -5,8 +5,10 @@ using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using ToDoApp.Interfaces;
+using ToDoApp.Models;
 
-namespace ToDoApp
+namespace ToDoApp.Logic
 {
     public class ToDoActions : IToDoActions
     {
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using ToDoApp.Logic;
 
 namespace ToDoApp
 {
